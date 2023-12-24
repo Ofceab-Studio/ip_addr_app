e884e1586d94b7822c5396307a00efc89922b852
yayahc <hamedcuenca5@gmail.com>
2023-12-24 04:12:34 +0000

    home widget added to app

---
+++ android/app/src/main/kotlin/com/example/ip_addr_show/WidgetProvider.kt
@@ -0,0 +1,23 @@
+package com.example.ip_addr_show  // your package name
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetBackgroundIntent
+import es.antonborri.home_widget.HomeWidgetLaunchIntent
+import es.antonborri.home_widget.HomeWidgetProvider
+
+class WidgetProvider : HomeWidgetProvider() {
+     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray, widgetData: SharedPreferences) {
+        appWidgetIds.forEach { widgetId ->
+            val views = RemoteViews(context.packageName, R.layout.widget_layout).apply {
+
+                val ipValue = widgetData.getString("ipValue", null)
+                setTextViewText(R.id.ipValue, ipValue)
+            }
+            appWidgetManager.updateAppWidget(widgetId, views)
+        }
+    }
+}
--- lib/features/ip_addr/cubit/ip_addr_cubit.dart
+++ lib/features/ip_addr/cubit/ip_addr_cubit.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home_widget/home_widget.dart';
 import 'package:injectable/injectable.dart';
 import 'package:ip_addr_show/core/modules/network_module.dart';
 import 'package:ip_addr_show/features/ip_addr/cubit/ip_addr_state.dart';
@@ -18,9 +18,16 @@ class IpAddrCubit extends Cubit<IpAddrState> {
       final ip = await netw.getWifiIP();
       if (ip != null) {
         emit(IpAddrDone(ip));
+        _updateWidget(ip);
       } else {
         emit(IpAddrFailed());
       }
     });
+  }
+
+  Future<void> _updateWidget(String ip) async {
+    await HomeWidget.saveWidgetData<String>('ipValue', ip);
+    await HomeWidget.updateWidget(
+        androidName: "WidgetProvider", name: "WidgetProvider");
   }
 }
